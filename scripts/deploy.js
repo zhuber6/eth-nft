705cb5a9ed916436a02af57e994c015c776030fb
zjhuber <zhuber6@gmail.com>
2021-11-20 14:06:42 -0700

    implemented static SVG base64 encoded data

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,19 @@
-// Deploys our NFT smart contract
-
 const main = async () => {
   const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
   const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
 
+  // Call the function.
   let txn = await nftContract.makeAnEpicNFT()
+  // Wait for it to be mined.
   await txn.wait()
+  console.log("Minted NFT #1")
 
-  txn = await nftContract.makeAnEpicNFT()
-  await txn.wait()
+  // txn = await nftContract.makeAnEpicNFT()
+  // // Wait for it to be mined.
+  // await txn.wait()
+  // console.log("Minted NFT #2")
 };
 
 const runMain = async () => {
